83bba87e5fb3cffc0b78dd075ccd7f381061cb2e
Salvador Sanjuan <salvador.sanjuan@gmail.com>
2016-05-25 23:58:24 +0200

    feat updated security access rules to followers and responsible

--- __openerp__.py
+++ __openerp__.py
@@ -3,5 +3,6 @@
 	'description': 'Extend the Todo app to multiuser',
 	'author': 'Salvador',
 	'depends': ['todo_app'],
-	'data': ['todo_view.xml'],
+	'data': ['todo_view.xml',
+			 'security/todo_access_rules.xml'],
 }
