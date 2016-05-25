7f8844ac4d839e334eff8d38ccebe73a48d79582
Salvador Sanjuan <salvador.sanjuan@gmail.com>
2016-05-25 23:17:11 +0200

    feat multi user to-do

---
+++ __openerp__.py
@@ -0,0 +1,7 @@
+{
+	'name': 'Multiuser To-Do',
+	'description': 'Extend the Todo app to multiuser',
+	'author': 'Salvador',
+	'depends': ['todo_app'],
+	'data': ['todo_view.xml'],
+}
---
+++ todo_task.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from openerp import models, fields, api
+
+class TodoTask(models.Model):
+	_inherit = 'todo.task'
+	user_id = fields.Many2one('res.users', 'Responsible')
+	date_deadline = fields.Date('Deadline')
+	name = fields.Char(help="What needs to be done?")
+
+	@api.multi
+	def do_clear_done(self):
+		domain = [('is_done','=',True),
+			'|',('user_id','=',self.env.uid),
+			('user_id','=',False)]
+		done_recs = self.search(domain)
+		done_recs.write({'actie':False})
+		return True
+
