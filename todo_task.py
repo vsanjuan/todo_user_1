f19dc3a5eb3df03c0e0da08eec4e96fbb00fb55f
Salvador Sanjuan <salvador.sanjuan@gmail.com>
2016-05-25 23:31:49 +0200

    feat mailthread

--- todo_task.py
+++ todo_task.py
@@ -3,7 +3,8 @@
 from openerp import models, fields, api
 
 class TodoTask(models.Model):
-	_inherit = 'todo.task'
+	_name = 'todo.task'
+	_inherit = ['todo.task','mail.thread']
 	user_id = fields.Many2one('res.users', 'Responsible')
 	date_deadline = fields.Date('Deadline')
 	name = fields.Char(help="What needs to be done?")
